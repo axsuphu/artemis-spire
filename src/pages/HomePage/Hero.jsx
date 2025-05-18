35ea8469d0e6d5d4d783ab78b880c591e9f32b4f
Alyssa Suphunthuchat <axsuphu@gmail.com>
2025-05-18 15:39:45 -0700

    updated Hero responsiveness for mobile and 2xl

--- src/pages/HomePage/Hero.jsx
+++ src/pages/HomePage/Hero.jsx
@@ -10,18 +10,17 @@ const Hero = ({ getImagePath }) => {
     <>
       <section
         id="hero"
-        className="relative mb-5 flex h-auto w-screen max-w-1920 flex-col items-center bg-hero-image bg-cover bg-no-repeat lg:mb-0 lg:h-screen lg:max-h-650 lg:flex-row lg:items-start lg:justify-center lg:pt-16 xl:pt-10 2xl:max-h-950"
+        className="relative mb-5 flex h-auto max-h-fit w-screen max-w-1920 flex-col items-center bg-hero-image bg-cover bg-no-repeat lg:mb-0 lg:h-screen lg:flex-row lg:items-start lg:justify-center"
       >
-        <div className="mx-10 my-8 flex flex-col gap-32 font-bakbak text-white sm:mx-0 sm:my-8 sm:w-2/3 md:mx-0 md:mb-8 md:mt-14 md:w-11/12 md:gap-14 lg:absolute lg:left-32 lg:m-0 lg:w-2/3 lg:gap-24 lg:pl-4 xl:top-24 xl:w-7/12 xl:gap-28 xl:p-0 2xl:left-52 2xl:top-36 2xl:gap-36">
-          <div className="relative flex flex-col font-bakbak text-3xl leading-none text-white md:text-5xl xl:text-5xl 2xl:gap-6 2xl:text-7xl">
-            <h1>Next-Gen Casino Games That Keep Players Coming Back</h1>
+        <div className="mx-10 my-8 flex flex-col gap-48 font-bakbak text-white 2xl:left-52 2xl:mx-0 2xl:my-32 2xl:w-4/5 2xl:tracking-widest">
+          <div className="relative flex flex-col font-bakbak text-3xl text-white md:text-5xl xl:text-5xl 2xl:w-9/12 2xl:text-7xl">
+            <h1 className="">Next-Gen Casino Games That Players Want</h1>
           </div>
-          <div className="flex justify-end">
+          <div className="flex justify-end 2xl:justify-start">
             <div className="flex w-5/6 items-end">
-              <h2 className="lg:w-11/22 text-md mb-2 text-right leading-none sm:text-lg md:mb-6 md:w-4/5 md:text-2xl lg:w-2/3 lg:text-left xl:w-2/3 xl:pr-4 xl:text-2xl 2xl:w-3/5 2xl:pb-6 2xl:text-3xl">
-                Boost retention, maximize revenue, and trust in bulletproof
-                technology, all from the leading innovator in gaming
-                experiences.
+              <h2 className="lg:w-11/22 text-md mb-2 text-right leading-none sm:text-lg md:mb-6 md:w-4/5 md:text-2xl lg:w-2/3 lg:text-left xl:w-2/3 xl:pr-4 xl:text-2xl 2xl:w-2/3 2xl:text-4xl">
+                Boost retention, maximize revenue, and trust in our bulletproof
+                technology.
               </h2>
             </div>
           </div>
